98e20496c5d4ea66cef1fb761aad4781b0479cba
urkweenkayla <test@example.com>
2023-09-16 16:16:42 -0700

    added some placeholder pages, updated the scriptjs code for popups

--- script.js
+++ script.js
@@ -1,73 +1,16 @@
-
 addEventListener("DOMContentLoaded", (event) => {
   //assigned anonymous function directly to event handler
   document.getElementById("aboutButton").onclick = function () { navLink("about.html") }
   document.getElementById("pawNGButton").onclick = function () { navLink("pawNG.html") }
   document.getElementById("portfolioButton").onclick = function () { navLink("portfolio.html") }
-  //defined named function and assigned to event handler
-  
-  //x About Popup
-  document.getElementById("xAboutButton").onclick = showxAboutPopup; 
-  document.getElementById("closexAboutPopupButton").onclick = closexAboutPopup;
-  const xAboutPopup = document.getElementById("xAboutPopup");
-  //Explanation for this const being here: If you try to access an element in your HTML using document.getElementById("popup") before the HTML document is fully parsed and loaded, the JavaScript code won't be able to find that element, and it will return null.
-    
-  // Closes the popup when the the screen surrounding the popup is clicked
-    xAboutPopup.addEventListener("click", (event) => {
-      if (event.target === xAboutPopup) {
-        closexAboutPopup();
-      }
-    });
+  //Popup Stuff
 
-//x pawNG Popup
-
-  document.getElementById("xpawNGButton").onclick = showxpawNGPopup; 
-  document.getElementById("closexpawNGPopupButton").onclick = closexpawNGPopup;
-  const xpawNGPopup = document.getElementById("xpawNGPopup");
-
-     // Closes the popup when the the screen surrounding the popup is clicked
-     xpawNGPopup.addEventListener("click", (event) => {
-      if (event.target === xpawNGPopup) {
-        closexpawNGPopup();
-      }
-    });
-
-// min Welcome Popup
-document.getElementById("minWelcomeButton").onclick = showminWelcomePopup; 
-document.getElementById("closeminWelcomePopupButton").onclick = closeminWelcomePopup;
-const minWelcomePopup = document.getElementById("minWelcomePopup");
-
-   // Closes the popup when the the screen surrounding the popup is clicked
-   minWelcomePopup.addEventListener("click", (event) => {
-    if (event.target === minWelcomePopup) {
-      closeminWelcomePopup();
-    }
-  });
-
-// max Welcome Popup
-document.getElementById("maxWelcomeButton").onclick = showmaxWelcomePopup; 
-document.getElementById("closemaxWelcomePopupButton").onclick = closemaxWelcomePopup;
-const maxWelcomePopup = document.getElementById("maxWelcomePopup");
-
-   // Closes the popup when the the screen surrounding the popup is clicked
-   maxWelcomePopup.addEventListener("click", (event) => {
-    if (event.target === maxWelcomePopup) {
-      closemaxWelcomePopup();
-    }
-  });
-
-// x Welcome Popup
-  document.getElementById("xWelcomeButton").onclick = showxWelcomePopup; 
-  document.getElementById("closexWelcomePopupButton").onclick = closexWelcomePopup;
-  const xWelcomePopup = document.getElementById("xWelcomePopup");
-
-     // Closes the popup when the the screen surrounding the popup is clicked
-     xWelcomePopup.addEventListener("click", (event) => {
-      if (event.target === xWelcomePopup) {
-        closexWelcomePopup();
-      }
-    });
-
+  registerPopup("open-popup",function (event){
+    showPopup(event,true)
+  })
+  registerPopup("close-popup",function(event){
+    showPopup(event,false)
+  })
 });
 
 //Function that allows button to be clicked to bring user to a new page
@@ -75,75 +18,35 @@ function navLink(link) {
   window.location.href = link
 }
 
-// ABOUT - Functions to show and close pop-up
-function showxAboutPopup() {
-  xAboutPopup.style.display = "block"
+
+function registerPopup(className, onClickFunction){
+    // Get all of the elements that have the "close-popup" class on them.
+    let buttontElements = document.getElementsByClassName(className)
+
+    // Loop through all of the buttons and add click event listeners
+    for (let i = 0; i < buttontElements.length; i++) {
+      let buttonElement = buttontElements[i]
+      buttonElement.addEventListener("click", onClickFunction.bind())
+    }
 }
 
-function closexAboutPopup() {
-  xAboutPopup.style.display = "none"
-}
+function showPopup(event,show){
 
-//pawNG - Functions to show and close pop-up
-function showxpawNGPopup() {
-  xpawNGPopup.style.display = "block"
-}
+  // Set display to "block" to show and "none" to hide the popup
+  let display = "none"
+  if (show){
+    display ="block"
+  }
 
-function closexpawNGPopup() {
-  xpawNGPopup.style.display = "none"
-}
-
-//minWelcome - Functions to show and close pop-up
-function showminWelcomePopup() {
-  minWelcomePopup.style.display = "block"
-}
-
-function closeminWelcomePopup() {
-  minWelcomePopup.style.display = "none"
-}
-
-//maxWelcome - Functions to show and close pop-up
-function showmaxWelcomePopup() {
-  maxWelcomePopup.style.display = "block"
-}
-
-function closemaxWelcomePopup() {
-  maxWelcomePopup.style.display = "none"
-}
-
-//xWELCOME - Functions to show and close pop-up
-function showxWelcomePopup() {
-  xWelcomePopup.style.display = "block"
-}
-
-function closexWelcomePopup() {
-  xWelcomePopup.style.display = "none"
+  let buttonElement = event.target
+  let popupId = buttonElement.dataset.popupId
+  let popupElement = document.getElementById(popupId)
+  if (popupElement){
+    popupElement.style.display = display
+  }
 }
 
 
 
-//maxWelcome - Functions to show and close pop-up
 
-
-// Functions that bring up the alter windows. Will eventually not need these. 
-
-// function closeAbout() {
-//   alert("I'll try not to take that personally.")
-// }
-
-// function closepawNG() {
-//   alert("What did you expect?")
-// }
-
-// function closeWelcome() {
-//   alert("This button does nothing but talk. It is considering a career in politics.")
-// }
-
-// function maxWelcome() {
-//   alert("You may be disappointed, but you'll never be as disappointed as my father.")
-// }
-
-// function minWelcome() {
-//   alert("When you light a match, do you expect it to start raining? Then why continue to click these buttons and expect a different result.")
-// }
 
