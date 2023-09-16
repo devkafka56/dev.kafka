07e8d64082b045bd8cb6b369245fe06feb993156
urkweenkayla <test@example.com>
2023-09-15 17:11:19 -0700

    added custom alert boxes inter alia

--- script.js
+++ script.js
@@ -3,12 +3,67 @@ addEventListener("DOMContentLoaded", (event) => {
   document.getElementById("aboutButton").onclick = function () { navLink("about.html") }
   document.getElementById("pawNGButton").onclick = function () { navLink("pawNG.html") }
   //defined named function and assigned to event handler
-  document.getElementById("xAboutButton").onclick = showPopup; 
-  document.getElementById("closePopupButton").onclick = closePopup;
-  //I'll need to add a new one of these for each of the unique messages that I want, I think. Maybe not.  
-  const popup = document.getElementById("popup");
-  //Question: why does the anonymous function work for the about and pawNG buttons, but not for the show and close Popup ones?
-  //I was confusing my close functions below with the functions in the event listener. I think if I wanted the popup to work the same as the other buttons I would need to remove them from the listener and just keep the functions here (similar to the close, max and min buttons).
+  
+  //x About Popup
+  document.getElementById("xAboutButton").onclick = showxAboutPopup; 
+  document.getElementById("closexAboutPopupButton").onclick = closexAboutPopup;
+  const xAboutPopup = document.getElementById("xAboutPopup");
+  //Explanation for this const being here: If you try to access an element in your HTML using document.getElementById("popup") before the HTML document is fully parsed and loaded, the JavaScript code won't be able to find that element, and it will return null.
+    // Closes the popup when the the screen surrounding the popup is clicked
+    xAboutPopup.addEventListener("click", (event) => {
+      if (event.target === xAboutPopup) {
+        closexAboutPopup();
+      }
+    });
+
+//x pawNG Popup
+
+  document.getElementById("xpawNGButton").onclick = showxpawNGPopup; 
+  document.getElementById("closexpawNGPopupButton").onclick = closexpawNGPopup;
+  const xpawNGPopup = document.getElementById("xpawNGPopup");
+
+     // Closes the popup when the the screen surrounding the popup is clicked
+     xpawNGPopup.addEventListener("click", (event) => {
+      if (event.target === xpawNGPopup) {
+        closexpawNGPopup();
+      }
+    });
+
+// min Welcome Popup
+document.getElementById("minWelcomeButton").onclick = showminWelcomePopup; 
+document.getElementById("closeminWelcomePopupButton").onclick = closeminWelcomePopup;
+const minWelcomePopup = document.getElementById("minWelcomePopup");
+
+   // Closes the popup when the the screen surrounding the popup is clicked
+   minWelcomePopup.addEventListener("click", (event) => {
+    if (event.target === minWelcomePopup) {
+      closeminWelcomePopup();
+    }
+  });
+
+// max Welcome Popup
+document.getElementById("maxWelcomeButton").onclick = showmaxWelcomePopup; 
+document.getElementById("closemaxWelcomePopupButton").onclick = closemaxWelcomePopup;
+const maxWelcomePopup = document.getElementById("maxWelcomePopup");
+
+   // Closes the popup when the the screen surrounding the popup is clicked
+   maxWelcomePopup.addEventListener("click", (event) => {
+    if (event.target === maxWelcomePopup) {
+      closemaxWelcomePopup();
+    }
+  });
+
+// x Welcome Popup
+  document.getElementById("xWelcomeButton").onclick = showxWelcomePopup; 
+  document.getElementById("closexWelcomePopupButton").onclick = closexWelcomePopup;
+  const xWelcomePopup = document.getElementById("xWelcomePopup");
+
+     // Closes the popup when the the screen surrounding the popup is clicked
+     xWelcomePopup.addEventListener("click", (event) => {
+      if (event.target === xWelcomePopup) {
+        closexWelcomePopup();
+      }
+    });
 
 });
 
@@ -17,15 +72,55 @@ function navLink(link) {
   window.location.href = link
 }
 
-// Function to show the pop-up
-function showPopup() {
-  popup.style.display = "block"
+// ABOUT - Functions to show and close pop-up
+function showxAboutPopup() {
+  xAboutPopup.style.display = "block"
 }
 
-// Function to close the pop-up
-function closePopup() {
-  popup.style.display = "none"
+function closexAboutPopup() {
+  xAboutPopup.style.display = "none"
 }
+
+//pawNG - Functions to show and close pop-up
+function showxpawNGPopup() {
+  xpawNGPopup.style.display = "block"
+}
+
+function closexpawNGPopup() {
+  xpawNGPopup.style.display = "none"
+}
+
+//minWelcome - Functions to show and close pop-up
+function showminWelcomePopup() {
+  minWelcomePopup.style.display = "block"
+}
+
+function closeminWelcomePopup() {
+  minWelcomePopup.style.display = "none"
+}
+
+//maxWelcome - Functions to show and close pop-up
+function showmaxWelcomePopup() {
+  maxWelcomePopup.style.display = "block"
+}
+
+function closemaxWelcomePopup() {
+  maxWelcomePopup.style.display = "none"
+}
+
+//xWELCOME - Functions to show and close pop-up
+function showxWelcomePopup() {
+  xWelcomePopup.style.display = "block"
+}
+
+function closexWelcomePopup() {
+  xWelcomePopup.style.display = "none"
+}
+
+
+
+//maxWelcome - Functions to show and close pop-up
+
 
 // Functions that bring up the alter windows. Will eventually not need these. 
 
